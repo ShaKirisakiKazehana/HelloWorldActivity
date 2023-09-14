6a331b553942eb7353bf2421cd7efe79a9ef4346
Dongjue Xie <x5176518@gmail.com>
2023-09-14 12:21:14 -0400

    added button stuff

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,25 +2,31 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.widget.Button
 import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
-    lateinit var displayTextView: TextView
+    lateinit var myButton: Button
+    lateinit var displayMessageTextView: TextView
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
-        displayTextView = findViewById(R.id.displayTextView)
-
-        
+        displayMessageTextView = findViewById(R.id.displayTextView)
+        myButton = findViewById<Button>(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
         // Respond to button click event per specifications
-
+        myButton.setOnClickListener{
+            displayMessageTextView.text = "Hello, $nameEditText.text"
+        }
 
     }
 }
